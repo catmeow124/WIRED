8cf524914371875fdeb0f67395c93c1803ef241e
catmeow124 <ttsstickman@gmail.com>
2026-08-10 14:22:40 -0400

    Update server.py

--- server.py
+++ server.py
@@ -10,15 +10,44 @@ server.listen(1)
 STORAGE = "STORAGE"
 SERVER_NAME = "SERVER1"
 
+
+def fetch(conn, path):
+    file_path = wired_path(path)
+
+    file_size = os.path.getsize(file_path)
+
+    conn.sendall(f"{file_size}\n".encode())
+
+    with open(file_path, "rb") as f:
+        while True:
+            fetch_data = f.read(4096)
+
+            if not fetch_data:
+                break
+
+            conn.sendall(fetch_data)
+
 def wired_path(location):
     prefix = "WIRED://" + SERVER_NAME + "/"
 
-    if location.startswith(prefix) == False:
+    if not location.startswith(prefix):
         raise ValueError("Location does not belong to this server :(")
 
     path = location[len(prefix):]
-    full_path = os.path.join(STORAGE, SERVER_NAME, path)
+
+    base_path = os.path.abspath(
+        os.path.join(STORAGE, SERVER_NAME)
+    )
+
+    full_path = os.path.abspath(
+        os.path.join(base_path, path)
+    )
+
+    if os.path.commonpath([base_path, full_path]) != base_path:
+        raise ValueError("Path escapes server storage")
+
     return full_path
+
 
 conn, addr = server.accept()
 print("[*] Connected:", addr)
@@ -35,14 +64,16 @@ if data.startswith("-"):
     print("Command:", command)
     print("Arguments:", arguments)
 
+    if command == "#":
+        pass
+
     if command == "-FETCH":
-        if len(arguments) > 1:
-            print("to many args for command")
+        if len(arguments) != 1:
+            print("wrong number of args for command")
         else:
             print("good fetch")
-            with open(wired_path(arguments[0])) as f:
-                print(f.read()) 
-conn.sendall(b"Hello from server!")
+            fetch(conn, arguments[0])
+
 
 conn.close()
 server.close()
